98bf478d55432490e15620a45c32f5f46db64b2b
alidaquluzade39-pixel <alidaquluzade39@gmail.com>
2026-08-26 16:30:08 +0400

    Fix import ordering in visualization package

--- src/scurator/visualization/__init__.py
+++ src/scurator/visualization/__init__.py
@@ -1,10 +1,6 @@
 """Visualization utilities for SCURATOR."""
 
-from .plots import (
-    load_mutation_data,
-    count_mutations_by_gene,
-    plot_mutations_by_gene,
-)
+from .plots import count_mutations_by_gene, load_mutation_data, plot_mutations_by_gene
 
 __all__ = [
     "load_mutation_data",
